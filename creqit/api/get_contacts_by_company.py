e45f06c038be7f216cb887239d9382746bf91518
alibahadirabra <127496341+alibahadirabra@users.noreply.github.com>
2025-05-22 14:05:54 +0300

    Create get_contacts_by_company.py

---
+++ creqit/api/get_contacts_by_company.py
@@ -0,0 +1,17 @@
+import creqit
+from creqit import _
+
+@creqit.whitelist()
+def get_contacts_by_company(company):
+    if not company:
+        return []
+
+    # Yetki kontrolü yapabilirsin burada, örn. sadece System Manager'lar erişsin
+    # if not creqit.has_permission("Contact_CRM"):
+    #     creqit.throw(_("You are not permitted to access Contact_CRM"))
+
+    contacts = creqit.get_all("Contact_CRM",
+        filters={"linkaccount": company},
+        fields=["name_surname", "title", "linkaccount", "department", "email", "phone", "mobilephone", "fax", "lastactivity"]
+    )
+    return contacts
