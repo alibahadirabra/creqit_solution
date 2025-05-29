be49c9fa2e25f02af26d29344ead50eb1730693e
alibahadirabra <127496341+alibahadirabra@users.noreply.github.com>
2025-05-29 22:08:57 +0300

    Update get_contacts_by_company.py

--- creqit/api/get_contacts_by_company.py
+++ creqit/api/get_contacts_by_company.py
@@ -11,12 +11,8 @@ def get_contacts_by_company(contact_name=None, company=None):
     else:
         return []
 
-    # Yetki kontrolü yapabilirsin burada, örn. sadece System Manager'lar erişsin
-    # if not creqit.has_permission("Contact_CRM"):
-    #     creqit.throw(_("You are not permitted to access Contact_CRM"))
-
     contacts = creqit.get_all("Contact_CRM",
-        filters={"linkaccount": company},
-        fields=["name_surname", "title", "linkaccount", "department", "email", "phone", "mobilephone", "fax", "lastactivity"]
+        filters=filters,
+        fields=["name", "name_surname", "title", "linkaccount", "department", "email", "phone", "mobilephone", "fax", "lastactivity"]
     )
     return contacts
