b61aeac29a1efcbc232fb1d28b53dbb7fe325475
alibahadirabra <127496341+alibahadirabra@users.noreply.github.com>
2025-05-29 17:53:22 +0300

    Update get_contacts_by_company.py

--- creqit/api/get_contacts_by_company.py
+++ creqit/api/get_contacts_by_company.py
@@ -3,7 +3,12 @@ from creqit import _
 
 @creqit.whitelist()
 def get_contacts_by_company(contact_name=None, company=None):
-    if not company:
+    filters = {}
+    if contact_name:
+        filters['name'] = contact_name
+    elif company:
+        filters['linkaccount'] = company
+    else:
         return []
 
     # Yetki kontrolü yapabilirsin burada, örn. sadece System Manager'lar erişsin
